97e4f8ddad6cd766eebf0df1af29300595b8ca9a
Pyro-YT <tmaganti@icloud.com>
2020-06-08 15:07:50 +0100

    Added stats tasks/120s

--- events/events.py
+++ events/events.py
@@ -4,6 +4,7 @@ import time
 import sys
 import os
 import json
+import pathlib
 
 class Events(commands.Cog):
 
@@ -18,6 +19,40 @@ class Events(commands.Cog):
     async def change_statuss(self):
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"DArealServers"))
 
+    @tasks.loop(seconds=120)
+    async def counter(self):
+        guild = await self.bot.fetch_guild(699991602126389248)
+
+        Files_channel = await self.bot.fetch_channel(719546043121008642)
+        Lines_channel = await self.bot.fetch_channel(719546162436374628)
+        Classes_channel = await self.bot.fetch_channel(719546254937423964)
+        Functions_channel = await self.bot.fetch_channel(719546329281593386)
+        Coroutines_channel = await self.bot.fetch_channel(719546488082006160)
+
+        p = pathlib.Path('./')
+        cm = cr = fn = cl = ls = fc = 0
+        for f in p.rglob('*.py'):
+            if str(f).startswith("venv"):
+                continue
+            fc += 1
+            with f.open(encoding='utf8') as of:
+                for l in of.readlines():
+                    l = l.strip()
+                    if l.startswith('class'):
+                        cl += 1
+                    if l.startswith('def'):
+                        fn += 1
+                    if l.startswith('async def'):
+                        cr += 1
+                    if '#' in l:
+                        cm += 1
+                    ls += 1
+        await Files_channel.edit(name=f'Files: {fc}')
+        await Lines_channel.edit(name=f'Lines: {ls}')
+        await Classes_channel.edit(name=f'Classes: {cl}')
+        await Functions_channel.edit(name=f'Functions: {fn}')
+        await Coroutines_channel.edit(name=f'Coroutines: {cr}')
+        # return f"file: {fc}\nline: {ls}\nclass: {cl}\nfunction: {fn}\ncoroutine: {cr}\ncomment: {cm}"
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -30,6 +65,7 @@ class Events(commands.Cog):
 
         self.change_status.start()
         self.change_statuss.start()
+        self.counter.start()
 
 
 def setup(bot):
